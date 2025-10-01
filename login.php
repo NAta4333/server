8e4ee3491ff680be72fa5968f1833804d5db1641
NAta4333 <saravinaleksandr8@gmail.com>
2025-10-02 00:47:26 +0300

    Delete login.php

--- login.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Получить данные из POST
-$username = $_POST['username'] ?? '';
-$password = $_POST['password'] ?? '';
-// Проверить в БД, сравнить password_verify
-// ... ваш код ...
-echo json_encode(['success'=>true, 'token'=>'demoToken']);
